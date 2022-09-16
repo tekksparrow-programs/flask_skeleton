0afd42372fe3dd3b85a1220ccc2d54c3f7068c55
Tekk Sparrow <tekksparrowprograms@gmail.com>
2022-09-16 13:39:46 +0700

    adds templates struct and about route

--- easy_api_app.py
+++ easy_api_app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World! This is the EASY API APP</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=12031, debug=True)
