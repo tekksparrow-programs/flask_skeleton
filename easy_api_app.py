9fec265ff18626901fa15df131230118541a0c7f
Tekk Sparrow <tekksparrowprograms@gmail.com>
2022-09-16 15:59:57 +0700

    adds layouts, bootstrap, removes ie conditionals

--- easy_api_app.py
+++ easy_api_app.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+data = [
+    { 
+        'element': 1,
+        'word': 'tuna fish'
+    },
+    {
+        'element': 2,
+        'word': 'windy',
+    },
+    {
+        'element': 3,
+        'word': 'wonderful'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', data=data, title='Home Page')
 
 @app.route("/about")
 def about_page():
-    return render_template('about.html')
+    return render_template('about.html', data=data, title='About Page')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=12031, debug=True)
