ba7a81d98aa7982048b8a8f3c81591631884c3d8
Tekk Sparrow <tekksparrowprograms@gmail.com>
2022-09-18 14:23:44 +0700

    adds routes/templates for login and register pages

--- easy_api_app.py
+++ easy_api_app.py
@@ -1,30 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 data = [
     { 
-        'element': 1,
-        'word': 'tuna fish'
+        'username': 'sleepyTurtle',
+        'hero': 'Tracer',
+        'date_added': 'Fri Sep 16 2022',
+        'info': 'won last game with positive k/d ratio'
     },
     {
-        'element': 2,
-        'word': 'windy',
+        'username': 'TekkSparrow',
+        'hero': 'Winston',
+        'date_added': 'Fri Sep 16 2022',
+        'info': 'lost their last 5 games',
     },
     {
-        'element': 3,
-        'word': 'wonderful'
+        'username': 'zing-ding',
+        'hero': 'Widow',
+        'date_added': 'Fri Sep 16 2022',
+        'info': 'no games played last 90 days'
     }
 ]
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html', data=data, title='Home Page')
+    return render_template('home.html', data=data, title='Home')
 
 @app.route("/about")
 def about_page():
-    return render_template('about.html', data=data, title='About Page')
+    return render_template('about.html', data=data, title='About')
+
+@app.route("/login")
+def login_page():
+    return render_template('login.html', data=data, title='Login')
+
+@app.route("/register")
+def register_page():
+    return render_template('register.html', data=data, title='Register')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=12031, debug=True)
